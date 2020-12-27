45848296442211baac3b423b8952e4e9ddb4256c
GAJENDRA SINGH THAKUR <71806431+gajendra1405@users.noreply.github.com>
2020-12-27 21:31:50 +0530

    Create instadpdownload.py

---
+++ instadpdownload.py
@@ -0,0 +1,13 @@
+'''
+Download Display Picture of an Instagram Account with Python
+Author: Gajendra Singh Thakur
+'''
+
+#import the necessary module!
+import instaloader
+#create an instance
+test = instaloader.Instaloader()
+#fetch the account details
+acc = input('Enter the Account Username: ')
+#download the data
+test.download_profile(acc, profile_pic_only = True)  
